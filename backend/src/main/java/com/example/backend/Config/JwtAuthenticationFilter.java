dd6c3247c2b9d6de75be612fb2fa2488095a1fba
dlaw96@hotmail.com <dlaw96@hotmail.com>
2023-06-19 17:16:58 -0400

    Created some more methods to handle the password

--- backend/src/main/java/com/example/backend/Config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/backend/Config/JwtAuthenticationFilter.java
@@ -3,9 +3,11 @@ package com.example.backend.Config;
 import java.io.IOException;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -38,7 +40,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
           UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+          if(jwtService.isTokenValid(jwt, userDetails)){
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+              userDetails, 
+              null, 
+              userDetails.getAuthorities()
+            );
+            authToken.setDetails(
+              new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+          }
         }
+        filterChain.doFilter(request, response);
   }
   
 }
