ead5dfd1c916f58a90f5bc366f715e46b13aa6e2
dlaw96@hotmail.com <dlaw96@hotmail.com>
2023-06-19 17:08:33 -0400

    Created new classes

--- backend/src/main/java/com/example/backend/Config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/backend/Config/JwtAuthenticationFilter.java
@@ -3,6 +3,9 @@ package com.example.backend.Config;
 import java.io.IOException;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
@@ -31,6 +35,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+          UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
   }
   
 }
