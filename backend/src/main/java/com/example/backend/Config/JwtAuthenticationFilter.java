5c58cc2918d4f51326fc5eed4ed07fcbf39b4e74
dlaw96@hotmail.com <dlaw96@hotmail.com>
2023-06-19 16:44:26 -0400

    Created JwtService

--- backend/src/main/java/com/example/backend/Config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/backend/Config/JwtAuthenticationFilter.java
@@ -16,16 +16,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+  private final JwtService jwtService;
+
   @Override
   protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
       throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
+
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
           filterChain.doFilter(request, response);
           return;
         }
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
   }
   
 }
