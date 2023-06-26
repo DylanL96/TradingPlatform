dd4eade98cffcc847f65a2758370a4351e679c2b
dlaw96@hotmail.com <dlaw96@hotmail.com>
2023-06-26 09:38:07 -0400

    Fixed portfolio and User class link

--- backend/src/main/java/com/example/backend/Models/User.java
+++ backend/src/main/java/com/example/backend/Models/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -24,7 +25,8 @@ public class User {
   private String email;
   private String password;
 
-  @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "portfolio_id")
   private Portfolio portfolio;
 
   public void addToPortfolio(Stock stock) {
