129338baba1e4fa233aac07e5b07988e4c1b6c2c
dlaw96@hotmail.com <dlaw96@hotmail.com>
2023-06-26 15:07:06 -0400

    users can now see portfolio holdings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,40 @@ import StockTradingPlatform from './Components/StockTradingPlatform';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const API_BASE_URL = 'http://localhost:8080/api/stocks/2/portfolio';
+const API_BASE_URL = 'http://localhost:8080/api/stocks/4/portfolio';
 
 const App = () => {
   const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const loadAsyncStuff = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get(API_BASE_URL);
         const jsonData = response.data;
 
         setData(jsonData);
-        console.log(jsonData);
+        setLoading(false);
       } catch (error) {
         console.log(error);
+        setLoading(false);
       }
     };
 
-    loadAsyncStuff();
+    fetchData();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  } 
 
   return (
     <div className="App">
-      <StockTradingPlatform data={data} />
+      {data ? (
+        <StockTradingPlatform data={data.stocks} />
+      ) : (
+        <div>No data available</div>
+      )}
     </div>
   );
 };
--- frontend/src/Components/StockTradingPlatform.js
+++ frontend/src/Components/StockTradingPlatform.js
@@ -1,63 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const StockTradingPlatform = () => {
-  // const API_BASE_URL = 'http://localhost:8080/api/stocks';
-  // const [stocks, setStocks] = useState([]);
-  // const [selectedStock, setSelectedStock] = useState('');
-  // const [quantity, setQuantity] = useState(0);
-
-  // useEffect(() => {
-  //   fetchStocks();
-  // }, []);
-
-  // const fetchStocks = () => {
-  //   getStocks()
-  //     .then(response => {
-  //       setStocks(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.log('Error fetching stocks:', error);
-  //     });
-  // };
-
-  // const handleBuyStock = () => {
-  //   const stockToBuy = stocks.find(stock => stock.symbol === selectedStock);
-  //   if (!stockToBuy) {
-  //     console.log('Stock not found');
-  //     return;
-  //   }
-
-  //   const buyRequest = {
-  //     stockId: stockToBuy.id,
-  //     quantity: quantity
-  //   };
-
-  //   buyStock(userId, buyRequest)
-  //     .then(response => {
-  //       console.log('Stock bought successfully');
-  //       // Perform any necessary updates or refresh data
-  //     })
-  //     .catch(error => {
-  //       console.log('Error buying stock:', error);
-  //     });
-  // };
-
-  // const handleSellStock = () => {
-  //   // Similar logic as handleBuyStock, but using the sellStock() function
-  // };
+const StockTradingPlatform = ({data}) => {
 
   return (
     <div>
-      <h1>Stock Trading Platform</h1>
-      {/* <select value={selectedStock} onChange={event => setSelectedStock(event.target.value)}>
-        <option value="">Select a stock</option>
-        {stocks.map(stock => (
-          <option key={stock.id} value={stock.symbol}>{stock.name}</option>
-        ))}
-      </select>
-      <input type="number" value={quantity} onChange={event => setQuantity(event.target.value)} />
-      <button onClick={handleBuyStock}>Buy</button>
-      <button onClick={handleSellStock}>Sell</button> */}
+      <h1>Stock Data</h1>
+      {data && (
+        <ul>
+          {Object.entries(data).map(([symbol, quantity]) => (
+            <li key={symbol}>
+              Symbol: {symbol}, Quantity: {quantity}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
