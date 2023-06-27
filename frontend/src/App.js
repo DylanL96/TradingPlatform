730c1cbb1a88c10be03d71a80e9f723344b915b9
dlaw96@hotmail.com <dlaw96@hotmail.com>
2023-06-26 22:11:48 -0400

    Creating login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,12 @@
-import './App.css';
-import StockTradingPlatform from './Components/StockTradingPlatform';
+import React from 'react';
 import Login from './Components/Login';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-const API_BASE_URL = 'http://localhost:8080/api/stocks/4/portfolio';
 
 const App = () => {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(API_BASE_URL);
-        const jsonData = response.data;
-
-        setData(jsonData);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  } 
 
   return (
-    <div className="App">
-      {data ? (
-        <StockTradingPlatform data={data.stocks} />
-      ) : (
-        <div>No data available</div>
-      )}
+    <div>
+      <h1>Stock Trading Platform</h1>
       <Login/>
     </div>
   );
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,46 +1,85 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import isEmpty from 'validator/lib/isEmpty';
+import isEmail from 'validator/lib/isEmail';
 
-const Login = ({ onLogin }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    errorMessage: false,
+    loading: false
+  });
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    setError('');
+  const {email, password, errorMessage, loading} = formData;
 
-    try {
-      const response = await axios.post('http://localhost:8080/authenticate', {
-        email,
-        password,
-      });
 
-      const { token } = response.data;
-      console.log(response.data)
-      // onLogin(userId, token);
-    } catch (error) {
-      setError('Incorrect email or password');
+  const handleChange = event => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+      errorMessage: ''
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('Sign in form submitted');
+    
+    if(isEmpty(email) || isEmpty(password)){
+      setFormData({
+        ...formData,
+        errorMessage: 'All fields are required'
+      })
+    } else if (!isEmail(email)){
+      setFormData({
+        ...formData,
+        errorMessage: 'Invalid email'
+      })
+    } else {
+
+      // setting the form data and creating a new iterable copy of the array using spread operator.
+      setFormData({...formData, loading: true})
+
+      // sending the destructured data object to the signin axios POST request
+      axios.post('http://localhost:8080/api/authenticate', formData)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+          setFormData({
+            ...formData,
+            errorMessage: 'Invalid email or password. Try again.'
+          })
+        })
     }
   };
 
-  return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+  const showSigninForm = () => (
+    <div className="signup-form">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+        <h2>Log in</h2>
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input name="email"  type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={handleChange}/>
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={handleChange}/>
         </div>
-        {error && <p>{error}</p>}
-        <button type="submit">Login</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
-  );
+  )
+  return (
+    <div>
+      {loading}
+      {errorMessage}
+      {showSigninForm()}
+    </div>
+  )
 };
 
 export default Login;
