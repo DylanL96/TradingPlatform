578935c747d85e2a003e4aad87ab85c7b2db5e7e
dlaw96@hotmail.com <dlaw96@hotmail.com>
2023-06-28 00:22:43 -0400

    Converted from then, catch to Try/Catch

--- frontend/src/Controller/StockController.js
+++ frontend/src/Controller/StockController.js
@@ -2,38 +2,37 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:8080/api';
 
-export const buyStock = (userID, symbol, quantity) => {
+export const buyStock = async (userID, symbol, quantity) => {
   const payload = {
     symbol,
     quantity
   };
 
-  return axios.post(`${API_BASE_URL}/stocks/${userID}/buy`, payload)
-    .then(response => {
-      console.log(response);
-      console.log("successfully bought stock");
-      return response.data; // Return the response data if needed
-    })
-    .catch(error => {
-      console.log(error);
-      throw error; // Throw the error to handle it in the component
-    });
+  try {
+    const response = await axios.post(`${API_BASE_URL}/stocks/${userID}/buy`, payload);
+    console.log(response);
+    console.log("successfully bought stock");
+    return response.data; // Return the response data if needed
+  } catch (error) {
+    console.log(error);
+    throw error; // Throw the error to handle it in the component
+  }
 };
 
-export const sellStock = (userID, symbol, quantity) => {
+
+export const sellStock = async (userID, symbol, quantity) => {
   const payload = {
     symbol,
     quantity
   };
 
-  return axios.post(`${API_BASE_URL}/stocks/${userID}/sell`, payload)
-    .then(response => {
-      console.log(response);
-      console.log("successfully sold stock");
-      return response.data; // Return the response data if needed
-    })
-    .catch(error => {
-      console.log(error);
-      throw error; // Throw the error to handle it in the component
-    });
+  try {
+    const response = await axios.post(`${API_BASE_URL}/stocks/${userID}/sell`, payload);
+    console.log(response);
+    console.log("successfully sold stock");
+    return response.data; // Return the response data if needed
+  } catch (error) {
+    console.log(error);
+    throw error; // Throw the error to handle it in the component
+  }
 };
